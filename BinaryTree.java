1fd96ce3ed35f954f31352006367eff15d54b060
PipoGonzalez59 <alexjosemilio@gmail.com>
2026-04-28 19:46:37 -0600

    implementacion de busqueda y recorrido in-order en el arbol

--- BinaryTree.java
+++ BinaryTree.java
@@ -29,3 +29,39 @@ public class BinaryTree {
 
         return current;
     }
+
+    public String search(String word) {
+        return searchRec(root, word.toLowerCase());
+    }
+
+    private String searchRec(Node current, String word) {
+
+        if (current == null) {
+            return null;
+        }
+
+        String key = current.data.getKey().toLowerCase();
+
+        if (word.equals(key)) {
+            return current.data.getValue();
+        }
+
+        if (word.compareTo(key) < 0) {
+            return searchRec(current.left, word);
+        } else {
+            return searchRec(current.right, word);
+        }
+    }
+
+    public void inOrder() {
+        inOrderRec(root);
+    }
+
+    private void inOrderRec(Node current) {
+        if (current != null) {
+            inOrderRec(current.left);
+            System.out.println(current.data);
+            inOrderRec(current.right);
+        }
+    }
+}
